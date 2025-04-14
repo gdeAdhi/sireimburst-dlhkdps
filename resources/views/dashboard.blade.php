4236a0155cdee01eafb33a2d92a989b3efbcbe4a
gdeAdhi <adhiyasadhiyo@yahoo.co.id>
2025-04-14 16:15:14 +0800

    editing dashboard pages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -46,7 +46,7 @@
               <div class="inner">
                 <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-                <p>Bounce Rate</p>
+                <p>Truck yang beroperasi</p>
               </div>
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
